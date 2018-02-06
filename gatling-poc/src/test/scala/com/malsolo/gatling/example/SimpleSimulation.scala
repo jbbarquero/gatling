a77171a25420b45cbd9042521e5a68783224c641
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-02-06 08:30:24 +0100

    gatling-poc: simulation with a spring boot project (TODO: include it)

--- gatling-poc/src/test/scala/com/malsolo/gatling/example/SimpleSimulation.scala
+++ gatling-poc/src/test/scala/com/malsolo/gatling/example/SimpleSimulation.scala
@@ -6,12 +6,30 @@ import io.gatling.http.Predef._
 
 class SimpleSimulation extends Simulation {
 
-  private val httpConfig = http.baseURL("http://computer-database.gatling.io")
+  private val httpConfig = http.baseURL("http://localhost:8080")
+    .contentTypeHeader("application/json")
 
   private val scn = scenario("SimpleSimulation").
-    exec(http("open").
-      get("/")).
-    pause(1)
+    exec(http("Check Health").
+      get("/actuator/health")
+      .check(status.is(200)))
+    .pause(1)
+    .exec(http("Create status")
+      .post("/status").body(StringBody(
+        """{
+          |    "name": "Gemma",
+          |    "description": "her",
+          |    "status": "true",
+          |    "amount": "2.0"
+          |}""".stripMargin))
+      .check(status.is(200))
+      .check(jsonPath("$.id").saveAs("statusId")))
+    .pause(1)
+    .exec(http("Ask for status")
+      .get("/status/${statusId}")
+      .check(status.is(200))
+      .check(jsonPath("$..id").is("${statusId}")))
+    .pause(1)
 
   setUp(scn.inject(atOnceUsers(1))).protocols(httpConfig)
 }
